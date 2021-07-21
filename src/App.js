705d1817b712ab7f8b98ef847b384ae01eab8dc3
nhatmvo <minhnhatvo97@gmail.com>
2021-07-21 08:21:35 +0700

    add PatientResultCard components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import PatientSearchResults from './components/Patient/PatientSearchResults';
 
 function App() {
+  const mockData = [
+    {
+      name: "Võ Minh Nhật",
+      dateOfBirth: "1997",
+      phoneNumber: "0344145250",
+      mostReccentExaminationDate: "01/10/2021",
+      numberOfEntries: 2
+    },
+    {
+      name: "Cù Khôi Nguyên",
+      dateOfBirth: "1997",
+      phoneNumber: "0932832483",
+      mostReccentExaminationDate: "10/05/2020",
+      numberOfEntries: 1
+    },
+    {
+      name: "Luyện Thị Yên",
+      dateOfBirth: "1997",
+      phoneNumber: "0388696963",
+      mostReccentExaminationDate: "12/03/2019",
+      numberOfEntries: 3
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PatientSearchResults results={mockData} />
     </div>
   );
 }
