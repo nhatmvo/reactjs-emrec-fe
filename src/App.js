04bd447c7f60d36ecef23ac187b0d13d6521ec38
Cù Khôi Nguyên <nguyenparker223@gmail.com>
2021-07-26 11:20:04 +0700

    Commit code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import EMRHeader from "./components/EMRHeader/EMRHeader";
+import EMRSearchBox from "./components/EMRSearchBox/EMRSearchBox";
+import { useState } from "react";
+import EMRInput from "./components/EMRInput/EMRInput";
+import "./components/common/common.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <EMRHeader />
+      <EMRSearchBox />
+      <div className="emr-form" style={{ margin: "0px 50px 50px" }}>
+        <div className="form-header">Header</div>
+        <div className="row">
+          <div className="section-title">Section title</div>
+          <hr></hr>
+        </div>
+        <div className="row">
+          <EMRInput
+            title="Textarea"
+            required={true}
+            example="V.D: đau đầu, sốt cao,..."
+            error="Lỗi"
+            type="1"
+          />
+          <EMRInput
+            title="Text"
+            required={true}
+            example="V.D: đau đầu, sốt cao,..."
+            error="Lỗi"
+            type="2"
+          />
+        </div>
+        <div className="row">
+          <EMRInput
+            title="Date"
+            required={true}
+            example="V.D: đau đầu, sốt cao,..."
+            error="Lỗi"
+            type="3"
+          />
+        </div>
+        <div className="row">
+          <EMRInput
+            title="Number"
+            required={true}
+            example="V.D: đau đầu, sốt cao,..."
+            error="Lỗi"
+            type="4"
+          />
+        </div>
+        <div className="row">
+          <EMRInput title="Image" required={true} error="Lỗi" type="5" />
+        </div>
+        <div className="row">
+          <EMRInput title="Radio" required={true} error="Lỗi" type="6" />
+        </div>
+        <div className="row">
+          <button className="button-layout" style={{ width: "150px" }}>
+            Button
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
